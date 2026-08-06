d8f23594dc2ee73bdf70c99030af0efa0fb6d4a9
Anna Luiza Santos Costa <anna.l.costa6@aluno.senai.br>
2026-08-06 14:05:14 -0300

    alterado

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,27 @@
+//IMPORTA O TIPO APPLICATIONCONFIG, USADO PARA CONFIGURAR A APLICAÇÃO ANGULAR
 import { ApplicationConfig } from '@angular/core';
+
+//IMPORTA A FUNÇÃO QUE REGISTRA AS ROTAS DA APLICAÇÃO
 import { provideRouter } from '@angular/router';
+
+//IMPORTA A FUNÇÃO QUE HABILITA O HTTPCLIENT PARA FAZER REQUISIÇÕES HTTP
 import { provideHttpClient } from '@angular/common/http';
 
+//IMPORTA O ARQUIVO ONDE ESTÃO DEFINIDAS AS ROTAS DA APLICAÇÃO
 import { routes } from './app.routes';
 
+//CONFIGURAÇÃO PRINCIPAL DA APLICAÇÃO
 export const appConfig: ApplicationConfig = {
+
+  //LISTA DE SERVIÇOS (PROVIDERS) DISPONÍVEIS PARA TODA A APLICAÇÃO
   providers: [
-    
-  provideRouter(routes)
-  ,
-  provideHttpClient()
-]
+
+    // REGISTRA AS ROTAS DEFINIDAS EM APP.ROUTES.TS
+    provideRouter(routes),
+
+    // HABILITA O HTTPCLIENT PARA REALIZAR REQUISIÇÕES HTTP
+    // (GET, POST, PUT, DELETE E ETC)
+    provideHttpClient()
+
+  ]
 };
